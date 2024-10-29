32e9e84aaec4044cbf5978692fca694475267ac9
Nadder Ragi <93446325+folanelfolany@users.noreply.github.com>
2024-10-29 13:26:57 +0000

    Pending changes exported from your codespace

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/about.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a software engineer skilled in the MERN stack, with a passion for creating user-friendly applications. I have experience in UI/UX design, and I'm always looking for new ways to make tech accessible to all.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "Back-end development",
+  "Testing",
   "Mobile user interfaces",
   "Graphic design",
+
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I'm a full stack software engineer who thrives on crafting seamless user experiences and building rock-solid backend systems. With a knack for juggling various programming languages and frameworks, I love turning innovative ideas into reality.";
 
 const About = () => {
   return (
